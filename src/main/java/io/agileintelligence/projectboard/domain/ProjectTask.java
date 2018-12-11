1ea2f4d496817268540882375d76972d6f951bab
Kristijan <kristija.junuzovski@codelogiq.com>
2018-12-11 10:19:18 +0100

    Project Board done.

---
+++ src/main/java/io/agileintelligence/projectboard/domain/ProjectTask.java
@@ -0,0 +1,74 @@
+package io.agileintelligence.projectboard.domain;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "project_task")
+public class ProjectTask implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "summary")
+    @NotBlank(message = "Summary cannot be blank!")
+    private String summary;
+
+    @Column(name = "acceptanceCriteria")
+    private String acceptanceCriteria;
+
+    @Column(name = "status")
+    private String status;
+
+    public ProjectTask() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAcceptanceCriteria() {
+        return acceptanceCriteria;
+    }
+
+    public void setAcceptanceCriteria(String acceptanceCriteria) {
+        this.acceptanceCriteria = acceptanceCriteria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", summary=" + summary + ", acceptanceCriteria=" + acceptanceCriteria + "]";
+    }
+}
